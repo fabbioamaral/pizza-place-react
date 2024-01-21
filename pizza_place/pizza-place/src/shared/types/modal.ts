177127ead2872cbfd5440d09370d0bd45faf8b5b
Amaral, Fabio <fabioamaral.93jr@gmail.com>
2024-01-22 05:23:35 +0800

    adds new optional property to the modal interface

---
+++ pizza_place/pizza-place/src/shared/types/modal.ts
@@ -0,0 +1,7 @@
+export type ModalPropsType = {
+  isOpen: boolean;
+  onAction: (data?: any) => void; // when clicking on buttons such as 'Save', 'Create', 'Update', etc
+  onDismiss: () => void; // when clicking on 'Dismiss' button
+  onClose: () => void; // when modal actually closes
+  data?: any;
+};
