766e9c8f7023ebc14a65396971bb60d83b92d399
Amaral, Fabio <fabioamaral.93jr@gmail.com>
2024-02-05 17:20:13 +0800

    adds const size

---
+++ pizza_place/pizza-place/src/shared/constants/sizes.ts
@@ -0,0 +1,7 @@
+export const SIZES = [
+  { name: 'N/A', value: 'not_applicable' },
+  { name: 'Small', value: 'small' },
+  { name: 'Regular', value: 'regular' },
+  { name: 'Large', value: 'large' },
+  { name: 'Extra Large', value: 'extra_large' },
+];
