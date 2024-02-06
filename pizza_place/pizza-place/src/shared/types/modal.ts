348329c33e3dd13abd69c5db085ddff3a8ef3cec
Amaral, Fabio <fabioamaral.93jr@gmail.com>
2024-02-06 20:12:35 +0800

    adds title and description property to modal props type

--- pizza_place/pizza-place/src/shared/types/modal.ts
+++ pizza_place/pizza-place/src/shared/types/modal.ts
@@ -1,4 +1,6 @@
 export type ModalPropsType = {
+  title?: string;
+  description?: string;
   isOpen: boolean;
   onAction: (data?: any) => void; // when clicking on buttons such as 'Save', 'Create', 'Update', etc
   onDismiss: () => void; // when clicking on 'Dismiss' button
