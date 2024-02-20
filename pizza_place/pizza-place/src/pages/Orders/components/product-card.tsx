ae06641347957ca3508aeff8461841f2be288280
Amaral, Fabio <fabioamaral.93jr@gmail.com>
2024-02-20 17:17:19 +0700

    creates product-card component

---
+++ pizza_place/pizza-place/src/pages/Orders/components/product-card.tsx
@@ -0,0 +1,15 @@
+import { Product } from '../../Products/types/product';
+import AddCircleIcon from '@mui/icons-material/AddCircle';
+
+function ProductCard(props?: Product) {
+  return (
+    <>
+      <div className="flex flex-col items-center pt-2 px-4 cursor-pointer rounded border w-36 h-32 m-5">
+        <p className="font-bold mt-2">{props?.name}</p>
+        <AddCircleIcon sx={{ my: 3 }} />
+      </div>
+    </>
+  );
+}
+
+export default ProductCard;
