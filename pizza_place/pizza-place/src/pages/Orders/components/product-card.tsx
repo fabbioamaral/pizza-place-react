d549a1adfd2e7361a6935055f15719e5e2b43bd6
Amaral, Fabio <fabioamaral.93jr@gmail.com>
2024-02-27 16:13:03 +0700

    adds price to ProductCard component

--- pizza_place/pizza-place/src/pages/Orders/components/product-card.tsx
+++ pizza_place/pizza-place/src/pages/Orders/components/product-card.tsx
@@ -1,11 +1,17 @@
 import { Product } from '../../Products/types/product';
 import AddCircleIcon from '@mui/icons-material/AddCircle';
 
-function ProductCard(props: Product) {
+function ProductCard(
+  props: Product & { onAction: (product: Product) => void }
+) {
   return (
     <>
-      <div className="flex flex-col items-center pt-2 px-4 cursor-pointer rounded border w-36 h-32 m-5">
+      <div
+        className="flex flex-col items-center pt-2 px-4 cursor-pointer rounded border w-40 h-40 m-5"
+        onClick={() => props.onAction(props)}
+      >
         <p className="font-bold mt-2">{props?.name}</p>
+        <p className="mt-1">R$ {props?.price}</p>
         <AddCircleIcon sx={{ my: 3 }} />
       </div>
     </>
