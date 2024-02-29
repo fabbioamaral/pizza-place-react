6c34409504ad6970bea01beb7fc3a7205db9da46
Amaral, Fabio <fabioamaral.93jr@gmail.com>
2024-02-29 16:16:06 +0700

    fixes ClientInfo

--- pizza_place/pizza-place/src/pages/Clients/components/client-info.tsx
+++ pizza_place/pizza-place/src/pages/Clients/components/client-info.tsx
@@ -1,13 +1,15 @@
 import { Client } from '../types/client';
 
-export default function ClientInfo(props: Client) {
+export default function ClientInfo(props: { client: Client }) {
   return (
     <>
       <div>
-        <p className="mb-1">{props.name || 'Joao da Silva'}</p>
-        <p className="mb-1">{props.phone || '(31) 9999-9999'}</p>
+        <p className="mb-1">{props.client.name || 'Joao da Silva'}</p>
+        <p className="mb-1">{props.client.phone || '(31) 9999-9999'}</p>
         <div className="p-2 rounded bg-gray-200">
-          <p>{props.notes || 'Cliente costuma pedir para tirar cebola'}</p>
+          <p>
+            {props.client.notes || 'Cliente costuma pedir para tirar cebola'}
+          </p>
         </div>
       </div>
     </>
