fc52c89f86aa2c56d5ccd8ed57f69c29ab16f42a
Amaral, Fabio <fabioamaral.93jr@gmail.com>
2024-03-07 16:08:15 +0700

    removes placeholder on ClientInfo component

--- pizza_place/pizza-place/src/pages/Clients/components/client-info.tsx
+++ pizza_place/pizza-place/src/pages/Clients/components/client-info.tsx
@@ -6,16 +6,16 @@ export default function ClientInfo(props: { client: Client }) {
       <div>
         <div className="flex mb-1 pt-2 pl-2">
           <p className="font-bold">Client name:</p>
-          <p className="ml-1">{props.client.name || 'Joao da Silva'}</p>
+          <p className="ml-1">{props.client.name}</p>
         </div>
         <div className="flex mb-1 pt-2 pl-2">
           <p className="font-bold">Phone:</p>
-          <p className="ml-1">{props.client.phone || '(31) 9999-9999'}</p>
+          <p className="ml-1">{props.client.phone}</p>
         </div>
         <div>
           <p className="pl-2 font-bold">Notes:</p>
           <p className="bg-gray-300 p-2 pb-6 mt-1 mx-2 mb-3">
-            {props.client.notes || 'Cliente costuma pedir para tirar cebola'}
+            {props.client.notes || '-'}
           </p>
         </div>
       </div>
