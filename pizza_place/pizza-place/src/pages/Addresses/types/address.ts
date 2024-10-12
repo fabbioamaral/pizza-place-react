3a1fcd3292c8bfac4aa7832c9a2ca135974ceeff
Amaral, Fabio <fabioamaral.93jr@gmail.com>
2024-10-12 12:27:08 +1000

    tweaks on Address model

--- pizza_place/pizza-place/src/pages/Addresses/types/address.ts
+++ pizza_place/pizza-place/src/pages/Addresses/types/address.ts
@@ -1,12 +1,11 @@
 export interface Address {
-  id: number;
+  id?: number;
   street: string;
   number: string;
   suburb_id: number;
   city: string;
-  selected: boolean; // exists only on FE
+  selected?: boolean; // exists only on FE
   default: boolean;
+  additionalInfo?: string;
+  clientId?: number;
 }
-
-// 3: alterar o BE para incluir propriedade isDefault no Address - Done
-// TODO: propriedade isSelected existe apenas no FE, pois nao ha necessidade de registrar isso no BE.
