88f6c54a18ca823618d666c245e205878a4c3836
Amaral, Fabio <fabioamaral.93jr@gmail.com>
2024-10-27 10:51:38 +1000

    header - fix issue of having to click multiple times to go back to home

--- pizza_place/pizza-place/src/shared/components/header.tsx
+++ pizza_place/pizza-place/src/shared/components/header.tsx
@@ -4,9 +4,11 @@ import { Link } from 'react-router-dom';
 function Header() {
   return (
     <>
-      <Button variant="contained" type="button" sx={{ mt: 2, ml: 4 }}>
-        <Link to="/">Home</Link>
-      </Button>
+      <Link to="/">
+        <Button variant="contained" type="button" sx={{ mt: 2, ml: 4 }}>
+          Home
+        </Button>
+      </Link>
     </>
   );
 }
